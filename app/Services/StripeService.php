3cb345fe8e6f4bf4376cf8d43d636233595204c3
Gergely, Gaal <gaalgergely@yahoo.com>
2020-05-12 20:29:17 +0200

    Stripe 3d Secure payment (SCA)

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -60,6 +60,14 @@ class StripeService
 
             $confirmation = $this->confirmPayment($paymentIntendId);
 
+            if($confirmation->status === 'requires_action') {
+                $clientSecret = $confirmation->client_secret;
+
+                return view('stripe.3d-secure')->with([
+                    'clientSecret' => $clientSecret
+                ]);
+            }
+
             if($confirmation->status === 'succeeded') {
                 $name = $confirmation->charges->data[0]->billing_details->name;
                 $currency = strtoupper($confirmation->currency);
